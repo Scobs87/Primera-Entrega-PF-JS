b5210c4800a631ebea7e10b0e3ae714da0c37a76
hein <heeein@hotmail.com>
2022-06-09 23:41:15 -0700

    Desafio 2 JS

--- interactividad.js
+++ interactividad.js
@@ -1,113 +1,82 @@
-const carrito = [];
-
 class Disco {
-  constructor(artista, album, genero, precio) {
+  constructor(artista, album, genero, precio, arte) {
     this.artista = artista;
     this.album = album;
     this.genero = genero;
     this.precio = precio;
+    this.arte = arte;
   }
 }
 
-const disco1 = new Disco("Radiohead", "Ok Computer", "Rock Alternativo", 300);
-const disco2 = new Disco("Gorillaz", "Demon Days", "Rock Alternativo", 400);
+const disco1 = new Disco(
+  "Radiohead",
+  "Ok Computer",
+  "Rock Alternativo",
+  300,
+  "./Imagenes/ok computer.jpg"
+);
+const disco2 = new Disco(
+  "Gorillaz",
+  "Demon Days",
+  "Rock Alternativo",
+  400,
+  "./Imagenes/Gorillaz.jpg"
+);
 const disco3 = new Disco(
   "The Strokes",
   "Room On Fire",
   "Rock Alternativo",
-  350
+  350,
+  "./Imagenes/Room on Fire.jpg"
 );
 const disco4 = new Disco(
   "Interpol",
   "Turn On The Bright Lights",
   "Rock Alternativo",
-  400
+  400,
+  "./Imagenes/Interpol.jpg"
 );
-const disco5 = new Disco("Bjork", "Homogenic", "Electronica/Trip Hop", 500);
+const disco5 = new Disco(
+  "Bjork",
+  "Homogenic",
+  "Electronica/Trip Hop",
+  500,
+  "./Imagenes/bjork.jpg"
+);
+const disco6 = new Disco(
+  "Nine Inch Nails",
+  "Fragile",
+  "Rock Industrial",
+  450,
+  "./Imagenes/NIN.jpg"
+);
 
-const seleccionarDisco = function () {
-  const discoSeleccionado = prompt(
-    " Bienvenido a Tijuana Record Shop! Selecciona algun disco de los siguentes artistas: Radiohead, Gorillaz, The Strokes, Interpol o Bjork"
-  ).toLowerCase();
+const inventarioDiscos = [disco1, disco2, disco3, disco4, disco5, disco6];
+let carrito = [];
 
-  if (discoSeleccionado == "radiohead") {
-    alert("Uy! Se nos ha agotado este disco por el momento, una disculpa!");
-    seleccionarDisco2();
-  } else if (discoSeleccionado == "gorillaz") {
-    carrito.push(disco2);
-    mensajeDeSeleccion(disco2);
-  } else if (discoSeleccionado == "the strokes") {
-    carrito.push(disco3);
-    mensajeDeSeleccion(disco3);
-  } else if (discoSeleccionado == "interpol") {
-    alert("Uy! Se nos ha agotado este disco por el momento, una disculpa!");
-    seleccionarDisco2();
-  } else if (discoSeleccionado == "bjork") {
-    carrito.push(disco5);
-    mensajeDeSeleccion(disco5);
-  } else {
-    alert("Por favor, ingresa una opcion correcta");
-    seleccionarDisco2();
-  }
+const contenedor = document.querySelector("#contenedor");
+
+inventarioDiscos.forEach((disco) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+  
+        <img src="${disco.arte}" alt="" />
+        <h4 class="cardArtista">${disco.artista} </h4>
+        <p class="cardAlbum bold"> ${disco.album} </p>
+        <p class="cardGenero"> ${disco.genero} </p>
+        <span class="cardPrecio bold">$${disco.precio} </span>
+        <button data-id="${disco.artista} " class="botonSelect">Seleccionar disco</button>
+     
+  `;
+  contenedor.append(card);
+});
+
+const mostrarEvento = (e) => {
+  console.log(e.target.getAttribute("data-id"));
 };
 
-const seleccionarDisco2 = function () {
-  const discoSeleccionado = prompt(
-    " Selecciona otro de nuestros artistas: Radiohead, Gorillaz, The Strokes, Interpol o Bjork"
-  ).toLowerCase();
-
-  if (discoSeleccionado == "radiohead") {
-    alert("Uy! Se nos ha agotado este disco por el momento, una disculpa!");
-    seleccionarDisco2();
-  } else if (discoSeleccionado == "gorillaz") {
-    carrito.push(disco2);
-    mensajeDeSeleccion(disco2);
-  } else if (discoSeleccionado == "the strokes") {
-    carrito.push(disco3);
-    mensajeDeSeleccion(disco3);
-  } else if (discoSeleccionado == "interpol") {
-    alert("Uy! Se nos ha agotado este disco por el momento, una disculpa!");
-    seleccionarDisco2();
-  } else if (discoSeleccionado == "bjork") {
-    carrito.push(disco5);
-    mensajeDeSeleccion(disco5);
-  } else {
-    alert("Por favor, ingresa una opcion correcta");
-    seleccionarDisco2();
-  }
-};
-
-const mensajeDeSeleccion = function (disco) {
-  console.log(
-    disco.artista +
-      "-" +
-      disco.album +
-      " " +
-      "de genero" +
-      " " +
-      disco.genero +
-      " " +
-      "tiene un precio de" +
-      " " +
-      disco.precio +
-      " " +
-      "pesos."
-  );
-
-  if (confirm("Quieres llevarte otro disco?")) {
-    seleccionarDisco2();
-  } else {
-    console.log("Tu total de hoy es $" + totalDeCompra() + "pesos");
-    console.log(carrito);
-  }
-};
-
-const totalDeCompra = function () {
-  let sumaTotal = 0;
-  for (const elemento of carrito) {
-    sumaTotal += elemento.precio;
-  }
-  return sumaTotal;
-};
-
-seleccionarDisco();
+const boton = document.querySelectorAll(".botonSelect");
+boton.forEach((boton) => {
+  boton.addEventListener("click", mostrarEvento);
+});
